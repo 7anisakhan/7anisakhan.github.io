67c4a008a800b62371cc3825c92a65a521881bf3
Anisa Khan <7anisakhan@gmail.com>
2025-12-15 23:19:33 -0500

    Initial commit: React project with gitignore

---
+++ website/src/components/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>
+                Hi! I’m Anisa, an Electrical Engineering student at the University of Waterloo.
+                
+            </p>
+        </section>
+    );
+}
+
+export default About;
