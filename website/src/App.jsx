4ad5896755d58f382007a0adc694177f4289f726
Anisa Khan <7anisakhan@gmail.com>
2025-12-17 15:07:42 -0500

    Added fonts, updated About Me

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,9 +1,13 @@
-import About from './components/About';
+import "./App.css";
+import About from "./components/About";
 
 function App() {
     return (
-        <div>
-            <About />
+        <div className="page">
+            <div className="container">
+                <h1 className="name">Anisa Khan</h1>
+                <About />
+            </div>
         </div>
     );
 }
--- website/src/components/About.jsx
+++ website/src/components/About.jsx
@@ -1,9 +1,9 @@
 function About() {
     return (
         <section>
-            <h2>About Me</h2>
+           
             <p>
-                Hi! I’m Anisa, an Electrical Engineering student at the University of Waterloo.
+                Electrical engineering @ University of Waterloo
                 
             </p>
         </section>
