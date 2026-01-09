e1a1b7345f96d3602287954a01432379fd3d0bfd
Anisa Khan <7anisakhan@gmail.com>
2026-01-08 19:09:10 -0500

    Updated contact page

--- website/src/Pages/Contact.jsx
+++ website/src/Pages/Contact.jsx
@@ -3,11 +3,18 @@ function Contact() {
 
     return (
 
-        <section>
-        <h2>
-            Coming soon
-        </h2>
-        </section >
+        <section className="contact">
+            <h2>Contact</h2>
+            <p>Fill out the form below to get into contact</p>
+            <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__pwXlGpURTJCQjJEWVgxRzNUWTNFSjc5NEJNM1lWNS4u"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-button"
+            >
+            Contact Form
+                </a>
+
+        </section>
 
 
     );
