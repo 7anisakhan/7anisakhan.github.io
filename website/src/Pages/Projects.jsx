63f5722c34b800a40e509f5479915f9d8bc7a34f
Anisa Khan <7anisakhan@gmail.com>
2026-05-13 01:41:00 -0400

    update all pages to latest version

--- website/src/Pages/Projects.jsx
+++ website/src/Pages/Projects.jsx
@@ -1,15 +1,36 @@
+
+import deliriumBuddy from "../assets/deliriumBuddy.png";
+
 function Projects() {
 
     return (
 
         <section>
-            <h2>
-                Coming soon
-            </h2>
+            <h2>Projects</h2>
+
+
+            <div className="project">
+                <div className="project-content">
+
+                    <div className="project-text">
+                        <h2>Delirium Buddy</h2>
+                        <ul>
+                            <li> Designed and built an Arduino-based dimmable light with a call-button feature to help prevent hospital induced delirium </li>
+
+                            <li> Conducted literature review to identify key delirium risk factors and assess their impact to guide project's features </li>
+                        </ul>
+                    </div>
+
+                    <img src={deliriumBuddy}
+                        alt="Delirium Buddy"
+                    />
+                </div>
+            </div>
+            
         </section >
 
-
+        
     );
 }
 
-export default Projects;
+export default Projects ;
--- website/src/Pages/Skills.jsx
+++ website/src/Pages/Skills.jsx
@@ -3,55 +3,77 @@ import React from "react";
 function Skills() {
     return (
         <section id="skills">
+
             <h2>Skills</h2>
 
-            <div className="skills-category">
-                <h3>Programming Languages</h3>
-                <ul>
-                    <li>C++</li>
-                    <li>JavaScript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
+            <div className="skills-grid">
 
-                </ul>
-            </div>
+                
 
+                <div className="skills-category">
+                    <h3>Programming Languages</h3>
+                    <ul>
+                        <li>C++</li>
+                        <li>JavaScript</li>
+                        
+
+                    </ul>
+                </div>
+
+                <div className="skills-category">
+                    <h3>Certifications</h3>
+                    <ul>
+                        <li>AZ-900
+                        </li>
+                        <li>AI-900</li>
+
+                    </ul>
+                </div>
+
+                <div className="skills-category">
+                    <h3>Web Technologies</h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>React</li>
+                        <li>Vite</li>
+
+                    </ul>
+                </div>
+           
             <div className="skills-category">
                 <h3>Tools & Platforms</h3>
                 <ul>
                     <li>VS Code</li>
                     <li>Visual Studio</li>
-                    <li>Arduino</li>
+                    <li>Git</li>
+                    <li>Microsoft Office</li>
                 </ul>
             </div>
 
             <div className="skills-category">
                 <h3>Hardware & Prototyping</h3>
-                <ul>
+                    <ul>
+                    <li>Arduino</li>
                     <li>Breadboards</li>
                     <li>Oscilloscopes</li>
                     <li>Function Generators</li>
                 </ul>
             </div>
 
-            <div className="skills-category">
-                <h3>Productivity Tools</h3>
-                <ul>
-                    <li>Microsoft Word</li>
-                    <li>Microsoft Excel</li>
-                    <li>Microsoft PowerPoint</li>
-                </ul>
+                <div className="skills-category">
+                    <h3>Languages</h3>
+                    <ul>
+                        <li>English</li>
+                        <li>French (DELF B2)</li>
+                    </ul>
+                </div>
+              
+
+            
+
             </div>
 
-            <div className="skills-category">
-                <h3>Web Development & Portfolio Skills</h3>
-                <ul>
-                    <li>React / JSX</li>
-                    <li>CSS / Flexbox / Grid</li>
-                    <li>Vite</li>
-                    <li>Git & GitHub</li>
-                </ul>
-            </div>
         </section>
     );
 }
